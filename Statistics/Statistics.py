6e531d2538c6af0fd68a90cb3e37dfc0ae09b96e
jasneekchugh <chughjasneek1096@gmail.com>
2021-07-17 23:32:58 -0400

    Updated the Statistics

---
+++ Statistics/Statistics.py
@@ -0,0 +1,35 @@
+from Calculator.Calculator import Calculator
+from Statistics.Mean import get_mean
+from Statistics.Median import get_median
+from Statistics.Mode import get_mode
+from Statistics.Variance import get_variance
+from Statistics.StandardDeviation import get_standard_deviation
+
+
+
+class Statistics(Calculator):
+
+    def __init__(self):
+        pass
+
+    def stats_mean(self, data):
+        self.result = get_mean(data)
+        return self.result
+
+    def stats_median(self, data):
+        data.sort()
+        self.result = get_median(data)
+        return self.result
+
+    def stats_mode(self, data):
+        self.result = get_mode(data)
+        return self.result
+
+    def stats_variance(self, data):
+        self.result = get_variance(data)
+        return self.result
+
+    def stats_standard_deviation(self, data):
+        self.result = get_standard_deviation(data)
+        return self.result
+
